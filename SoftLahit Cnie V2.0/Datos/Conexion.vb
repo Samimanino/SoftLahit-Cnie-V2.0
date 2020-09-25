2e073bda21b0f050f777c89b02675eba5eb1a7ef
Samimanino <63593104+Samimanino@users.noreply.github.com>
2020-09-25 22:27:25 +0200

    Agregar archivos de proyecto.

---
+++ SoftLahit Cnie V2.0/Datos/Conexion.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Conexion
+
+    Protected Con As New SqlConnection
+    Protected cmd As New SqlCommand
+    Protected dat As New DataTable
+    Protected adap As New SqlDataAdapter
+    Protected Drd As SqlDataReader
+
+    Protected Function Conectar()
+        Try
+            Con = New SqlConnection(My.Settings.Con)
+            Con.Open()
+            Return True
+        Catch ex As SqlException
+            MsgBox(ex.Message)
+            Return False
+        End Try
+
+    End Function
+
+    Protected Function Desconectar()
+        Try
+            If Con.State = ConnectionState.Open Then
+                Con.Dispose()
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As SqlException
+            MsgBox(ex.Message)
+            Return False
+        End Try
+
+    End Function
+
+End Class
